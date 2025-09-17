88e42f2840a22d38eae6296e77b7c11dbb779f92
Surya Srikar <suryasrikar21@gmail.com>
2025-09-17 12:03:30 +0530

    Re-added correct SEA backend with proper .gitignore

---
+++ SEA/src/main/java/com/example/SEA/app/EmailRequest.java
@@ -0,0 +1,9 @@
+package com.example.SEA.app;
+
+import lombok.Data;
+
+@Data
+public class EmailRequest {
+    private String emailContent;
+    private String tone;
+}
---
+++ SEA/src/test/java/com/example/SEA/SeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SEA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
